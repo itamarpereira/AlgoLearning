14f1bd03a61d9459c331d01cef6269ef99104842
Itamar Pereira <itamartpereira@hotmail.com>
2020-06-09 12:00:25 -0300

    Add photo to users model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def user_image_url(user)
+    user.photo.attached? ? cl_image_path(user.photo.key) : 'https://images.unsplash.com/photo-1557318041-1ce374d55ebf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80'
+  end
 end
