aa4ec71cd2354219e7f3b5f2111fcedb18b95981
Itamar Pereira <itamartpereira@hotmail.com>
2020-06-10 16:17:25 -0300

    Fix image card using cloudinary

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
   end
 
   def course_image_url(course)
-    course.photo.attached? ? "url('#{cl_image_path(course.photo.key)}')" : "url(https://images.unsplash.com/photo-1557318041-1ce374d55ebf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80)"
+    course.photo.attached? ? "#{cl_image_path(course.photo.key)}" : "https://images.unsplash.com/photo-1557318041-1ce374d55ebf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
   end
 end
