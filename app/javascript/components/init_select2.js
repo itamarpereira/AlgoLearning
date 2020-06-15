8fb70ddb4505288bb1fda4312cca39fdb022d733
Viktor Araujo <vkaraujo00@gmail.com>
2020-06-15 16:41:36 -0300

    front-end improved, multiple tags can be added

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,7 +2,10 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2(); // (~ document.querySelectorAll)
+  $('.select2').select2({
+    placeholder: "Select a Tag",
+    allowClear: true
+  }); // (~ document.querySelectorAll)
 };
 
 export { initSelect2 };
