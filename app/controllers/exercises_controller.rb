4b5d71477a257d2ada8a95eeb488dee2cc46e948
flaviomahoney <flavio@mahoneyt.com>
2020-06-15 17:28:20 -0300

    Created Controller Exercises

---
+++ app/controllers/exercises_controller.rb
@@ -0,0 +1,27 @@
+class ExercisesController < ApplicationController
+	def new
+		@exercise = Exercise.new
+		@course = Course.find(params[:course_id])
+    authorize @exercise
+  end
+
+
+	def create
+		@exercise = Exercise.new(exercise_params)
+		@exercise.course = Course.find(params[:course_id])
+		authorize @exercise
+    if @exercise.save
+      flash[:success] = "Exercise Create Successfully"
+      redirect_to course_path(Course.find(params[:course_id]))
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+	end
+	
+	private
+
+  def exercise_params
+    params.require(:exercise).permit(:code)
+	end
+end
