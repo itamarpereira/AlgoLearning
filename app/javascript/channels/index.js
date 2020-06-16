16ca63cea292611cdbb085b4155cd351cc65a147
flaviomahoney <flavio@mahoneyt.com>
2020-06-16 11:34:22 -0300

    Adding collapsible into exercises

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,16 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+let myLabels = document.querySelectorAll('.lbl-toggle');
+
+Array.from(myLabels).forEach(label => {
+  label.addEventListener('keydown', e => {
+    // 32 === spacebar
+    // 13 === enter
+    if (e.which === 32 || e.which === 13) {
+      e.preventDefault();
+      label.click();
+    };
+  });
+});
