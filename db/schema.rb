e628431a639ed726a6026839781a39aa30351f67
Leonardo Figueira <leofigueiracorreia@yahoo.com.br>
2020-06-16 15:59:12 -0300

    extra assets to dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_191824) do
+ActiveRecord::Schema.define(version: 2020_06_16_131511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 2020_06_15_191824) do
   end
 
   create_table "progresses", force: :cascade do |t|
-    t.string "status"
+    t.string "status", default: "Not Started"
     t.bigint "user_id", null: false
     t.bigint "course_id", null: false
     t.datetime "created_at", precision: 6, null: false
