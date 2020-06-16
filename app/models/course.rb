f9b6173f789f9b3cfee545b2f0119580de53be1f
Itamar Pereira <itamartpereira@hotmail.com>
2020-06-16 18:19:57 -0300

    Add images to the slides

--- app/models/course.rb
+++ app/models/course.rb
@@ -9,7 +9,7 @@ class Course < ApplicationRecord
   has_many :exercises
 
   has_one_attached :photo
-  has_many_attached :photos
+  has_many_attached :images
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
