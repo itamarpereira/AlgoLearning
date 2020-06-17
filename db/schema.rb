d1a0b84d386bac06d00dbd9d2b112b4e617b2e07
flaviomahoney <flavio@mahoneyt.com>
2020-06-17 12:30:44 -0300

    Runned the migration and Difficulty string was created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_131511) do
+ActiveRecord::Schema.define(version: 2020_06_17_145911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2020_06_16_131511) do
     t.bigint "course_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "difficulty"
     t.index ["course_id"], name: "index_exercises_on_course_id"
   end
 
