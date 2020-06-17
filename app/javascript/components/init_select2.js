5d3527c68ff347eec2f9743f664b99e4efc6518b
Viktor Araujo <vkaraujo00@gmail.com>
2020-06-17 10:02:57 -0300

    select2 improved

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,10 +2,13 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2({
-    placeholder: "Select a Tag",
-    allowClear: true
-  }); // (~ document.querySelectorAll)
+  const select2 = document.querySelector(".select2")
+  if (select2) {
+    $('.select2').select2({
+      placeholder: "Select a Tag",
+      allowClear: true
+    }); // (~ document.querySelectorAll)
+  }
 };
 
 export { initSelect2 };
