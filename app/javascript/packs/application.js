35b0eefda8b48c1dc6cdc738c5fb7540b5012942
Viktor Araujo <vkaraujo00@gmail.com>
2020-06-17 12:33:23 -0300

    select 2 correction

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("trix")
+require("@rails/actiontext")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -24,7 +25,6 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import { loadDynamicBannerText } from '../components/banner';
 import "@rails/actiontext"
 
 
@@ -36,8 +36,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initSelect2();
-  loadDynamicBannerText();
 });
 
-require("trix")
-require("@rails/actiontext")
